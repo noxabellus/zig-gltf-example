ac3fbc932557d7af363a52d50e4c18fda9efd445
noxabellus <noxabellus@gmail.com>
2024-10-22 15:42:20 -0500

    added sdl and opengl bindings

--- build.zig
+++ build.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
+const sdl = @import("sdl");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    const zgl = b.dependency("zgl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const sdlSdk = sdl.init(b, .{});
 
     const exe = b.addExecutable(.{
         .name = "main",
@@ -11,9 +19,23 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.root_module.addImport("zgl", zgl.module("zgl"));
+
+    sdlSdk.link(exe, .dynamic, sdl.Library.SDL2);
+
+    exe.root_module.addImport("sdl", sdlSdk.getWrapperModule());
+
     const testExe = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
+
+    testExe.root_module.addImport("zgl", zgl.module("zgl"));
+
+    sdlSdk.link(testExe, .dynamic, sdl.Library.SDL2);
+
+    testExe.root_module.addImport("sdl", sdlSdk.getWrapperModule());
 
     b.default_step.dependOn(&b.addInstallArtifact(exe, .{}).step);
 
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,50 @@
 const std = @import("std");
+const zgl = @import("zgl");
+const sdl = @import("sdl");
 
 pub fn main() !void {
-    std.debug.print("Hello world", .{});
+    try sdl.init(.{
+        .video = true,
+        .events = true,
+    });
+    defer sdl.quit();
+
+    var window = try sdl.createWindow(
+        "zig-gltf",
+        .centered, .centered,
+        640, 480,
+        .{.vis = .shown, .context = .opengl },
+    );
+    defer window.destroy();
+
+    try sdl.gl.setAttribute(.{ .context_major_version = 3 });
+    try sdl.gl.setAttribute(.{ .context_minor_version = 1 });
+    try sdl.gl.setAttribute(.{ .context_profile_mask = .core });
+
+    const glContext = try sdl.gl.createContext(window);
+    defer glContext.delete();
+
+    try zgl.loadExtensions({}, sdlGlGetProcAddressWrapper);
+
+    try sdl.gl.setSwapInterval(.adaptive_vsync);
+
+    mainLoop: while (true) {
+        while (sdl.pollEvent()) |ev| {
+            switch (ev) {
+                .quit => break :mainLoop,
+                else => {},
+            }
+        }
+
+        zgl.clearColor(1.0, 0.0, 1.0, 1.0);
+        zgl.clear(.{ .color = true, .depth = true });
+
+        sdl.gl.swapWindow(window);
+    }
+}
+
+fn sdlGlGetProcAddressWrapper(_: void, symbolName: [:0]const u8) ?zgl.binding.FunctionPointer {
+    return sdl.gl.getProcAddress(symbolName);
 }
 
 test {
